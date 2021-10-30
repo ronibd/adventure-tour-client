656701fceb970313abce386f8447ee85eaec4db0
Abu Noman Roni <abunomanroni@gmail.com>
2021-10-30 16:44:25 +0600

    updated

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB52MRqa6YxzUixv7nQ2NtPrslqcDpHSao",
-  authDomain: "adven-tour.firebaseapp.com",
-  projectId: "adven-tour",
-  storageBucket: "adven-tour.appspot.com",
-  messagingSenderId: "243750349611",
-  appId: "1:243750349611:web:929cb3dea7e4327761f36e",
+  apiKey: "AIzaSyCfO65NIFGo5yJFq3k9pxT1sDNSQu2wOxA",
+
+  authDomain: "react-firebase-authentic-4d2e2.firebaseapp.com",
+
+  projectId: "react-firebase-authentic-4d2e2",
+
+  storageBucket: "react-firebase-authentic-4d2e2.appspot.com",
+
+  messagingSenderId: "142984826107",
+
+  appId: "1:142984826107:web:e4f46bff7ded1908ac345e",
 }
+
 export default firebaseConfig
